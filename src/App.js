40e664f9aac56aeffc5153106bdaa6c3cf9f47fd
hamzaboularbah <boularbah59@gmail.com>
2019-06-28 00:12:13 +0100

    added active class to tabs

--- src/App.js
+++ src/App.js
@@ -1,85 +1,109 @@
-import React from 'react';
+import React, { useState } from 'react'
 import styled from 'styled-components'
-
 
 const Container = styled.div`
   max-width: 1103px;
   margin: 0 auto;
   border-radius: 5px 5px 0px 0px;
-  overflow:hidden;
+  overflow: hidden;
 `
 const TabBar = styled.ul`
   margin: 0;
-  list-style:none;
-  background-color: #DFE1E5;
+  list-style: none;
+  background-color: #dfe1e5;
   height: 45px;
-  position: relative; 
+  position: relative;
 `
 const Tab = styled.li`
-  position:relative;
-  display:inline-block;
-  width:100%;
-  max-width:240px;
+  position: relative;
+  display: inline-block;
+  width: 100%;
+  max-width: 240px;
   height: 34px;
-  background-color: #DFE1E5;
+  background-color: #dfe1e5;
   bottom: -12px;
-  padding : 10px 0px 0px 10px; 
+  padding: 10px 0px 0px 10px;
 
-${props => props.active ? {
-    zIndex: 2,
-    borderRadius: "8px 8px 0px 0px",
-    boxShadow: "0 10px 20px rgba(0,0,0,.5)",
-    backgroundColor: "#FFFFFF",
-    "::after, ::before": {
-      position: "absolute",
-      content: "''",
-      background: "transparent",
-      width: "15px",
-      height: "15px",
-      borderRadius: "100%",
-      borderWidth: "10px",
-      top: "8px",
-      borderStyle: "solid",
-    },
-    "::after": {
-      borderColor: "transparent #FFFFFF transparent transparent",
-      transform: "rotate(48deg)",
-      left: "-25px",
-    },
-    "::before": {
-      borderColor: "transparent transparent transparent #FFFFFF",
-      transform: "rotate(-48deg)",
-      right: "-25px",
-    }
-  } : {
-      "::before": {
-        content: "''",
-        position: "absolute",
-        width: "1px",
-        height: "20px",
-        backgroundColor: "gray",
-        top: "0", bottom: "0", right: "10px",
-        margin: "auto",
-        opacity: ".7"
-
+  ${props =>
+    props.active
+      ? {
+        zIndex: 2,
+        borderRadius: '8px 8px 0px 0px',
+        boxShadow: '0 10px 20px rgba(0,0,0,.5)',
+        backgroundColor: '#FFFFFF',
+        '::after, ::before': {
+          position: 'absolute',
+          content: "''",
+          background: 'transparent',
+          width: '15px',
+          height: '15px',
+          borderRadius: '100%',
+          borderWidth: '10px',
+          top: '8px',
+          borderStyle: 'solid'
+        },
+        '::after': {
+          borderColor: 'transparent #FFFFFF transparent transparent',
+          transform: 'rotate(48deg)',
+          left: '-25px'
+        },
+        '::before': {
+          borderColor: 'transparent transparent transparent #FFFFFF',
+          transform: 'rotate(-48deg)',
+          right: '-25px'
+        }
       }
-    }}
+      : {
+        '::before': {
+          content: "''",
+          position: 'absolute',
+          width: '1px',
+          height: '20px',
+          backgroundColor: 'gray',
+          top: '0',
+          bottom: '0',
+          right: '10px',
+          margin: 'auto',
+          opacity: '.7'
+        }
+      }}
 `
 
 let App = () => {
+  let _tabs = [
+    { name: 'facebook', active: false },
+    { name: 'instagram', active: true },
+    { name: 'reddit', active: false },
+    { name: 'twitter', active: false }
+  ]
+  let [tabs, setTabs] = useState(_tabs)
+
+  let handleClick = tabName => {
+    setTabs(
+      tabs.map(tab =>
+        tabName === tab.name
+          ? { ...tab, active: true }
+          : { ...tab, active: false }
+      )
+    )
+  }
   return (
     <div className="App">
       <Container>
         <TabBar>
-          <Tab key={1}> Facebook</Tab>
-          <Tab key={2} active >Instagram</Tab>
-          <Tab key={3} >Twitter</Tab>
-          <Tab key={4} >Reddit</Tab>
+          {tabs.map((tab, i) => (
+            <Tab
+              onClick={() => handleClick(tab.name)}
+              active={tab.active ? true : false}
+              key={i}
+            >
+              {tab.name}
+            </Tab>
+          ))}
         </TabBar>
       </Container>
-
-    </div >
-  );
+    </div>
+  )
 }
 
-export default App;
+export default App
